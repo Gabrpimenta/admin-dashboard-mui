31e3e6f64f03450f911dec80e1e7648fffef8aa7
Gabrpimenta <gabrielrpimenta@gmail.com>
2023-02-20 10:20:11 -0300

    ✨ add Contacts, Invoices and Team sections

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { Routes, Route } from 'react-router-dom';
 import Topbar from './scenes/global/Topbar';
 import Sidebar from './scenes/global/Sidebar';
 import Dashboard from './scenes/dashboard';
-// import Team from './scenes/team';
-// import Invoices from './scenes/invoices';
-// import Contacts from './scenes/contacts';
+import Team from './scenes/team';
+import Invoices from './scenes/invoices';
+import Contacts from './scenes/contacts';
 // import Bar from './scenes/bar';
 // import Form from './scenes/form';
 // import Line from './scenes/line';
@@ -27,9 +27,11 @@ function App() {
             <Topbar />
             <Routes>
               <Route path='/' element={<Dashboard />} />
-              {/* <Route path='/team' element={<Team />} />
-              <Route path='/invoices' element={<Invoices />} />
+              <Route path='/team' element={<Team />} />
               <Route path='/contacts' element={<Contacts />} />
+              <Route path='/invoices' element={<Invoices />} />
+              {/* 
+              
               <Route path='/bar' element={<Bar />} />
               <Route path='/form' element={<Form />} />
               <Route path='/line' element={<Line />} />
