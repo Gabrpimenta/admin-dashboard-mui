6e8430104fe268a32ac0521129d0c7384e6096c6
Gabrpimenta <gabrielrpimenta@gmail.com>
2023-02-21 19:32:04 -0300

    ✨ Charts Pages complete

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ import Contacts from './scenes/contacts';
 import Form from './scenes/form';
 import Calendar from './scenes/calendar';
 import FAQ from './scenes/faq';
-// import Bar from './scenes/bar';
-// import Line from './scenes/line';
-// import Pie from './scenes/pie';
-// import Geography from './scenes/geography';
+import Bar from './scenes/bar';
+import Pie from './scenes/pie';
+import Line from './scenes/line';
+import Geography from './scenes/geography';
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -33,14 +33,10 @@ function App() {
               <Route path='/form' element={<Form />} />
               <Route path='/calendar' element={<Calendar />} />
               <Route path='/faq' element={<FAQ />} />
-              {/* 
-              
               <Route path='/bar' element={<Bar />} />
-              <Route path='/line' element={<Line />} />
               <Route path='/pie' element={<Pie />} />
-             
+              <Route path='/line' element={<Line />} />
               <Route path='/geography' element={<Geography />} />
-               */}
             </Routes>
           </main>
         </div>
